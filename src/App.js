c81edfb908ed511f4cc3f59c5f8f44ecc7b8e6e7
Marius <marius.horghidan@yahoo.com>
2023-03-17 11:17:05 +0200

    created the initial html structure

--- src/App.js
+++ src/App.js
@@ -4,13 +4,4 @@ export default class App {
   constructor(root) {
     this.root = root;
   }
-
-  render() {
-    const { root } = this;
-
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Boilerplate';
-
-    root.append(h1);
-  }
 }
